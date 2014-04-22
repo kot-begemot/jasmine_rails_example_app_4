4c5e61a6d04d47b4053583e22a47bf505ebc12f6
E-Max <max.zab87@gmail.com>
2014-04-22 20:15:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JasminriseExampleApp4::Application.config.secret_key_base = '4559a8ba703ba17946653a8e77bbaf7713a72476e32f4431d14d41382f4b1a2db23df00490098f22eac1db1b3d06482a609207efa2b00b729c7e3effa4f0faa2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JasminriseExampleApp4::Application.config.session_store :cookie_store, key: '_jasminrise_example_app_4_session'
